59e2187e0618bac89fc9aed169666bdb8b32e5b1
Christopher Sidebottom <christopher.sidebottom@bbc.co.uk>
2015-01-08 20:46:41 +0000

    Test our way to a defined set of rules for the Client

--- test/client.js
+++ test/client.js
@@ -8,22 +8,146 @@ var Sinon = require('sinon'),
 //    .get('https://api.eveonline.com/server/ServerStatus.xml.aspx')
 //    .pipe(parser.parse);
 
-describe('Fetch', function() {
-    var client;
+describe('Client', function() {
+    describe('Fetcher', function() {
+        var client;
 
-    beforeEach(function() {
-        client = new Client({ id: '1234', key: '2341abcdef' });
-        Sinon.stub(client._cache, 'get').yields();
-        Sinon.stub(client._cache, 'set').yields();
+        beforeEach(function() {
+            client = new Client({ key: '1234', code: '2341abcdef' });
+            Sinon.stub(client._cache, 'get').yields();
+            Sinon.stub(client._cache, 'set').yields();
+            Sinon.stub(Request, 'get').returns(FS.createReadStream(__dirname + '/fixtures/ServerStatus.xml'));
+        });
+
+        afterEach(function() {
+            client._cache.get.restore();
+            client._cache.set.restore();
+            Request.get.restore();
+        });
+
+        it('requests the correct URL for the endpoint with id and key added', function(done) {
+            client.fetch('char', 'CharacterSheet', {}, function() {
+                assert(Request.get.calledWith({
+                    url: 'https://api.eveonline.com/char/CharacterSheet.xml.aspx',
+                    qs: {
+                        keyID: '1234',
+                        vCode: '2341abcdef'
+                    }
+                }));
+                done();
+            });
+        });
+
+        it('merges the URL params into the request', function(done) {
+            client.fetch('char', 'CharacterSheet', { cake: 'bake' }, function() {
+                assert(Request.get.calledWith({
+                    url: 'https://api.eveonline.com/char/CharacterSheet.xml.aspx',
+                    qs: {
+                        keyID: '1234',
+                        vCode: '2341abcdef',
+                        cake: 'bake'
+                    }
+                }));
+                done();
+            });
+        });
+
+        it('Does not use the apiKey and ID when not required', function(done) {
+            client.fetch('server', 'ServerStatus', {}, function() {
+                assert(Request.get.calledWith({
+                    url: 'https://api.eveonline.com/server/ServerStatus.xml.aspx',
+                    qs: {}
+                }));
+                done();
+            });
+        });
+
+        it('respects the TTLs given by the Eve api', function(done) {
+            client.fetch('server', 'ServerStatus', {}, function() {
+                assert.equal(1, client._cache.set.getCall(0).args[2]);
+                done();
+            });
+        });
+
+        it('Stores the parsed form of the response', function(done) {
+            client.fetch('server', 'ServerStatus', {}, function() {
+                assert.deepEqual(require('./fixtures/ServerStatus'), client._cache.set.getCall(0).args[1]);
+                done();
+            });
+        });
     });
 
-    it('Should request the correct URL for the endpoint', function(done) {
-        var stream = FS.createReadStream(__dirname + '/fixtures/ServerStatus.xml');
-        Sinon.stub(Request, 'get').returns(stream);
+    describe('Cached response', function() {
+        var client;
 
-        client.fetch('server', 'ServerStatus', {}, function() {
-            assert(Request.get.calledWith('https://api.eveonline.com/server/ServerStatus.xml.aspx'));
-            done();
+        beforeEach(function() {
+            client = new Client({ key: '1234', code: '2341abcdef' });
+            Sinon.stub(Request, 'get').returns(FS.createReadStream(__dirname + '/fixtures/ServerStatus.xml'));
+        });
+
+        afterEach(function() {
+            Request.get.restore();
+        });
+
+        it('Combines the request resource, endpoint and parameters for the cache key', function(done) {
+            Sinon.stub(client._cache, 'get').yields();
+            Sinon.stub(client._cache, 'set').yields();
+
+            client.fetch('space', 'SHIP', { cake: 'bake' }, function() {
+                var key = 'space:SHIP:1234:2341abcdef:58225a3b0a5331d39b4bf58cb39d169b';
+
+                assert(client._cache.get.calledWith(key));
+                assert(client._cache.set.calledWith(key));
+                done();
+            });
+        });
+
+        it('returns a cached response if one is available', function(done) {
+            Sinon.stub(client._cache, 'get').yields(null, { cake: 'bake' });
+
+            client.fetch('space', 'SHIP', {}, function(err, result) {
+                assert(!Request.called);
+                assert.deepEqual({ cake: 'bake' }, result);
+                client._cache.get.restore();
+                done();
+            });
+        });
+
+        it('Propagates cache errors through to the client', function(done) {
+            Sinon.stub(client._cache, 'get').yields(new Error('cake'));
+
+            client.fetch('space', 'SHIP', {}, function(err) {
+                assert(!Request.get.called);
+                assert.equal('cake', err.message);
+                client._cache.get.restore();
+                done();
+            });
+        });
+    });
+
+    describe('Validation', function() {
+        it('errors on bad input arguments', function() {
+            assert.throws(
+                function() {
+                    new Client({
+                        cake: 'bake'
+                    });
+                },
+                /ValidationError/
+            );
+
+            assert.throws(
+                function() {
+                    new Client({
+                        cache: {
+                            engine: 'goblinsridingsharksfiringlasers'
+                        },
+                        key: 'validishstring',
+                        code: 'validishstring'
+                    });
+                },
+                /ValidationError/
+            );
         });
     });
 });
